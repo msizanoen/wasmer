79e97c83afddbae479dd67d101812083dd07c8ab
msizanoen <phanquangminh217@gmail.com>
2019-08-16 12:48:33 +0700

    Include libunwind

---
+++ libunwind/src/wasmer_ext.h
@@ -0,0 +1,10 @@
+void wasmer_unwind_armehabi_register(
+    void *text_begin,
+    size_t text_size,
+    void *aeidx_begin,
+    size_t aeidx_size
+);
+
+void wasmer_unwind_armehabi_deregister(
+    void *text_begin,
+);
