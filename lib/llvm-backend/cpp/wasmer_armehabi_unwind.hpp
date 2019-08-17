3f1429ce6da2ff16bb71a0eb681ef321c4a20c76
msizanoen <phanquangminh217@gmail.com>
2019-08-17 21:31:00 +0700

    Format headers

--- lib/llvm-backend/cpp/wasmer_armehabi_unwind.hpp
+++ lib/llvm-backend/cpp/wasmer_armehabi_unwind.hpp
@@ -1,6 +1,4 @@
-void wasmer_unwind_armehabi_register(
-    void *text_begin, size_t text_size,
-    void *axidx_begin, size_t axidx_size
-);
+void wasmer_unwind_armehabi_register(void *text_begin, size_t text_size,
+                                     void *axidx_begin, size_t axidx_size);
 
 void wasmer_unwind_armehabi_deregister(void *text_begin);
--- libunwind/src/wasmer_ext.h
+++ libunwind/src/wasmer_ext.h
@@ -1,10 +1,4 @@
-void wasmer_unwind_armehabi_register(
-    void *text_begin,
-    size_t text_size,
-    void *aeidx_begin,
-    size_t aeidx_size
-);
+void wasmer_unwind_armehabi_register(void *text_begin, size_t text_size,
+                                     void *aeidx_begin, size_t aeidx_size);
 
-void wasmer_unwind_armehabi_deregister(
-    void *text_begin,
-);
+void wasmer_unwind_armehabi_deregister(void *text_begin);
